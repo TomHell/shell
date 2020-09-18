71431038108c4715d9299ffadd07b0532dcade6f
TomHell84 <dilopho29@web.de>
2020-09-18 19:15:45 +0200

    start bash scripting

---
+++ hello.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# Themes
+# https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
+
+
+
+echo "Hello bash script"
+
+echo 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"'
